4e15b47d380fbb9b68b89916c167c7651d7a60a5
Tuğrul Topuz <tugrultopuz@gmail.com>
2020-03-31 14:32:59 +0300

    update to es6 format

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 
-var util = require('util'),
-Transform = require('stream').Transform;
+const Transform = require('stream').Transform;
 
-util.inherits(Aline, Transform);
+class Aline extends Transform {
+    constructor(options) {
+        super();
 
-function Aline(options) {
-    if (!(this instanceof Aline))
-     return new Aline(options);
+        this._tail = Buffer.alloc(0);
+        this._separator = (options && options.separator) || '\n';
+    }
+    
+    _transform(chunk, encoding, callback) {
+        const index = chunk.lastIndexOf(this._separator);
+        
+        if (index === chunk.length || index === -1) {
+            callback(null, Buffer.concat([this._tail, chunk]));
+            this._tail = Buffer.alloc(0);
+            return;
+        }
 
-    Transform.call(this, options);
-    this._tail = Buffer.alloc(0);
-    this._separator = (options && options.separator) || '\n';
+        const head = Buffer.concat([this._tail, chunk.slice(0, index)]);
+        this._tail = Buffer.from(chunk.slice(index + 1));
+
+        callback(null, head);
+    }
+    
+    _flush(callback) {
+        callback(null, this._tail);
+    }
 }
 
-Aline.prototype._transform = function (chunk, encoding, callback) {
-
-    var index = chunk.lastIndexOf(this._separator);
-
-    if (index === chunk.length) {
-        this._tail = Buffer.alloc(0);
-        return callback(null, chunk);
-    }
-
-    var head = Buffer.concat([this._tail, chunk.slice(0, index)]);
-    this._tail = Buffer.from(chunk.slice(index + 1));
-
-    callback(null, head);
-};
-
-Aline.prototype._flush = function(callback) {
-    callback(null, this._tail);
-};
 
 module.exports = Aline;
